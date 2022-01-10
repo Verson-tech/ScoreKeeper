cbfaa0ee2b7ad3569803355b43f96045cb377289
vira.murray <vira.avramenko@gmail.com>
2022-01-10 15:31:27 +0200

    wip

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-## 'The Web Developer Bootcamp 2022 by Colt' Steele
+## "The Web Developer Bootcamp 2022" by Colt Steele
 
 ## [UDEMY](https://www.udemy.com/)
+
+## [My project](https://verson-tech.github.io/ScoreKeeper/)
--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+
+colors = [
+    "#a3f72d",
+    "#fa0c24",
+    "##0cfa0c",
+    "#0af7d4",
+    "#f307f7",
+    "#1a2beb",
+    "#eb71de",
+    "#4eed32",
+    "#21fce3",
+    "#90f0f0",
+    "#7a1818",
+    "#e05024",
+    "#2cfa02",
+  ];
+
 const p1 = {
     score: 0,
     button: document.querySelector('#p1Button'),
@@ -25,6 +42,7 @@ function updateScores(player, opponent) {
             opponent.button.disabled = true;
         }
         player.display.textContent = player.score;
+        createRandomCircle()
     }
 }
 
@@ -51,5 +69,42 @@ function reset() {
         p.display.textContent = 0;
         p.display.classList.remove('has-text-success', 'has-text-danger');
         p.button.disabled = false;
-    }
+      }
 }
+
+
+// // ping pong live
+// function createRandomCircle() {
+//     const circle = document.createElement("div");
+//     const size = getRandomSize(10, 60);
+//     const playground = document.querySelector('#playground')
+//     const { width, height } = playground.getBoundingClientRect();
+//     const x = getRandomSize(0, width - size);
+//     const y = getRandomSize(0, height - size);
+  
+//     circle.classList.add("circle");
+//     circle.style.width = `${size}px`;
+//     circle.style.height = `${size}px`;
+//     circle.style.top = `${y}px`;
+//     circle.style.left = `${x}px`;
+  
+//     setColor(circle);
+  
+//     playground.append(circle);
+//   }
+
+
+//   function getRandomSize(min, max) {
+//     return Math.round(Math.random() * (max - min) + min);
+//   }
+
+//   function setColor(element) {
+//     const color = getRandomColor();
+//     element.style.background = color;
+//     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+//   }
+
+//   function getRandomColor() {
+//     const index = Math.floor(Math.random() * colors.length);
+//     return colors[index];
+//   }
